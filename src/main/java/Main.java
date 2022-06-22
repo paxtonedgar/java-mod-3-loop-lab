951a2d5758b3632273c817c3e4c053241540b652
Paxton Edgar <paxtonedgar3@gmail.com>
2022-06-22 14:39:02 -0400

    init push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,17 @@ public class Main {
         int startingYear = 2000;
         int targetYear = 2011;
         int currentYear = startingYear;
+        int year = 0; 
         while (currentYear < targetYear) {
-            System.out.println((currentYear - startingYear) + " year(s) have passed");
-            // conditional logic based on the current year
+            year = currentYear - startingYear; 
+            
+            if(year == 0){
+            }
+            else if(year == 1){
+                System.out.println(year + " year have passed");
+            }else{
+                System.out.println(year + " years have passed");
+            }
             currentYear++;
         }
     }
